0a22a7c993dd39be42da70f60601988d9c72b222
JONATAN PEREZ <jonatanperezsoy@gmail.com>
2017-04-21 17:15:20 -0500

    update Django 1.11

--- private_media/urls.py
+++ private_media/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from django.conf import settings
+from private_media.views import serve_private_fileRest, serve_private_file
 
 
 urlpatterns = [
@@ -8,17 +9,17 @@ urlpatterns = [
 #condition for validation of RESTFRAMEWORK
 if hasattr(settings,'PRIVATE_REST_FRAMEWORK_AUTH'):
 	if settings.PRIVATE_REST_FRAMEWORK_AUTH:
-		from private_media import views
-		additional_settings  += ['private_media.views',
-			url(r'^{0}(?P<path>.*)$'.format(settings.PRIVATE_MEDIA_URL.lstrip('/')), views.serve_private_fileRest.as_view()),
+		
+		additional_settings  += [
+			url(r'^{0}(?P<path>.*)$'.format(settings.PRIVATE_MEDIA_URL.lstrip('/')), views.serve_private_fileRest,),
 		]
 	else:
 		additional_settings += ['private_media.views',
-			url(r'^{0}(?P<path>.*)$'.format(settings.PRIVATE_MEDIA_URL.lstrip('/')), 'serve_private_file',),
+			url(r'^{0}(?P<path>.*)$'.format(settings.PRIVATE_MEDIA_URL.lstrip('/')), serve_private_file,),
 		] 
 else:
     additional_settings += ['private_media.views',
-    	url(r'^{0}(?P<path>.*)$'.format(settings.PRIVATE_MEDIA_URL.lstrip('/')), 'serve_private_file',),
+    	url(r'^{0}(?P<path>.*)$'.format(settings.PRIVATE_MEDIA_URL.lstrip('/')),serve_private_file,),
 	]
 
 
